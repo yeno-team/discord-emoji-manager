d264e313efd2839bfbcf1bcc061842d3781756a1
khai93 <discordisherenow@gmail.com>
2019-05-18 10:05:11 +0000

    home view test

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import HomeView from 'views/Home';
 import NotFound from 'components/core/NotFound';
 
 export default [
-  { path: '/', component: HomeView },
+  { path: '/home', component: HomeView },
   { path: '*', component: NotFound },
 ];
